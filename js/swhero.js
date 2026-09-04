dde4cad054cf57504dbf646d1cc6a68561d23a65
geumnahyeo3--code <geumnahyeon3@gmail.com>
2026-09-04 16:53:44 +0900

    feat: 히어로 슬라이드 영역 스와이퍼 라이브러리 작업중

---
+++ js/swhero.js
@@ -0,0 +1,10 @@
+const sehero = new Swiper(".sw-hero", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".hero-next-btn",
+    prevEl: ".hero-prev-btn",
+  },
+});
