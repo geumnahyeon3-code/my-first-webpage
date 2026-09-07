bd6445eae4618d9a00e7a42bed9547675bdb6ae0
geumnahyeo3--code <geumnahyeon3@gmail.com>
2026-09-07 16:40:08 +0900

    스와이퍼 구현중

--- js/swhero.js
+++ js/swhero.js
@@ -1,4 +1,14 @@
+const mainvissualbg = document.querySelector(
+  ".hero .main-visual .main-visual-bg",
+);
+// console.log(mainvissualbg);
+
 const sehero = new Swiper(".sw-hero", {
+  speed: 1000,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -6,5 +16,18 @@ const sehero = new Swiper(".sw-hero", {
   navigation: {
     nextEl: ".hero-next-btn",
     prevEl: ".hero-prev-btn",
+  },
+
+  on: {
+    slidchangeTransitionStart: function (swiper) {
+      // const activeslide = swiper.activeIndex + 1;
+      // console.log(activeslide);
+      // mainvissualbg.style.backgrondimage = `URL(assets/images/slide_${activeslide}.png)`;
+      const activeslide = swiper.slides[swiper.activeindex];
+      // console.log(activeindex);
+      const activeslideimg = activeslide.querySelector(".slide-image img");
+      // console.log(activeslideimg.src);
+      mainvisualbg.style.backgrondimage = `url;(${activeslideimg.src})`;
+    },
   },
 });
